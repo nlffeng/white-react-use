3c93808c292aba2ef46c21a78469ed6528939f83
nlffeng <nlffeng@163.com>
2021-11-07 22:24:11 +0800

    test: [useInterval] 完善测试用例

--- test/useInterval.test.ts
+++ test/useInterval.test.ts
@@ -28,14 +28,14 @@ describe('useInterval', () => {
     const callback = jest.fn()
 
     const { result, rerender } = renderHook(() => {
-      useInterval(callback, 800, { immediate: true })
+      useInterval(callback, undefined, { immediate: true })
     })
 
     expect(callback).toBeCalled()
     expect(callback).toHaveBeenCalledTimes(1)
 
     // “快进”时间使得所有定时器回调被执行
-    jest.advanceTimersByTime(800)
+    jest.advanceTimersByTime(1000)
 
     expect(callback).toBeCalled()
     expect(callback).toHaveBeenCalledTimes(2)
@@ -43,17 +43,58 @@ describe('useInterval', () => {
 
   it('定时器的状态', () => {
     const callback = jest.fn()
+    let _getStatus: Function = () => {}
+    let _handle: Function = () => {}
 
     const { result, rerender } = renderHook(() => {
-      const { getStatus, handle } = useInterval(callback, 800, { immediate: true })
+      const { getStatus, handle } = useInterval(callback, 800)
+      _getStatus = getStatus
+      _handle = handle
     })
 
-    expect(callback).toBeCalled()
-    expect(callback).toHaveBeenCalledTimes(1)
+    expect(callback).not.toBeCalled()
+    expect(_getStatus?.()).toBe('doing')
+
+    _handle?.('stop')
+
+    expect(callback).not.toBeCalled()
+    expect(_getStatus?.()).toBe('stopping')
 
     // “快进”时间使得所有定时器回调被执行
     jest.advanceTimersByTime(800)
 
+    expect(callback).not.toBeCalled()
+    expect(_getStatus?.()).toBe('stopping')
+
+    _handle('do', true)
+  })
+
+  it('手动操作定时器', () => {
+    const callback = jest.fn()
+    let _getStatus: Function = () => {}
+    let _handle: Function = () => {}
+
+    const { result, rerender } = renderHook(() => {
+      const { getStatus, handle } = useInterval(callback, 800)
+      _getStatus = getStatus
+      _handle = handle
+    })
+
+    expect(_getStatus?.()).toBe('doing')
+
+    _handle?.('stop')
+
+    expect(_getStatus?.()).toBe('stopping')
+
+    _handle?.('do', true)
+
+    expect(_getStatus?.()).toBe('doing')
+    expect(callback).toBeCalled()
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    _handle?.('do', true)
+
+    expect(_getStatus?.()).toBe('doing')
     expect(callback).toBeCalled()
     expect(callback).toHaveBeenCalledTimes(2)
   })
